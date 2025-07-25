75fb96a3ef73d288a70b5a78fe4695d03cd167c7
Subhadeep-Coder <dharsubhadeep5@gmail.com>
2025-07-25 17:10:01 +0530

    Updated few files.

--- src/delivery-agent/auth/guards/google-auth.guard.ts
+++ src/delivery-agent/auth/guards/google-auth.guard.ts
@@ -1,12 +1,2 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
-@Injectable()
-export class GoogleCustomerAuthGuard extends AuthGuard('google-customer') { }
-
-
-@Injectable()
-export class GoogleSellerAuthGuard extends AuthGuard('google-seller') { }
-
-@Injectable()
-export class GoogleAdminAuthGuard extends AuthGuard('google-admin') { }
